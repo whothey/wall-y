2ec73da32ff33d5fd917fc5e5f1bba7d2ed06fa9
Gabriel Henrique Rudey <ucraaa@gmail.com>
2017-10-19 18:11:54 -0200

    Sync

--- src/cgrobot/Camera.hpp
+++ src/cgrobot/Camera.hpp
@@ -10,13 +10,15 @@ class Camera
     GLdouble m_eyeX, m_eyeY, m_eyeZ,
              m_cntX, m_cntY, m_cntZ,
              m_upX , m_upY , m_upZ,
-             m_rotation;
+             m_vrotation, m_hrotation;
 public:
     Camera(GLdouble x, GLdouble y, GLdouble z);
     void position(GLdouble x, GLdouble y, GLdouble z);
     void lookAt(GLdouble x, GLdouble y, GLdouble z);
     void up(GLdouble x, GLdouble y, GLdouble z);
-    void turn(GLdouble dg);
+    void turn(GLdouble dgh, GLdouble dgv);
+    void turnH(GLdouble dg);
+    void turnV(GLdouble dg);
     void activate();
 };
 }
